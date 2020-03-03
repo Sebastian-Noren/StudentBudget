3d9c6e9d99808356f3aa1b4a5345232f1c9466ae
Sebastian <sebastian.martensson95@gmail.com>
2020-03-03 21:01:54 +0100

    Budget development

---
+++ app/src/main/java/se/hkr/studentbudget/budget/BudgetDialog.java
@@ -0,0 +1,113 @@
+package se.hkr.studentbudget.budget;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+
+import java.util.Objects;
+
+import se.hkr.studentbudget.AppConstants;
+
+import se.hkr.studentbudget.R;
+import se.hkr.studentbudget.transactions.CategoryRowItem;
+
+
+public class BudgetDialog extends DialogFragment {
+    private String tag = "Info";
+    private TextView inputAmountText;
+    private Spinner spinnerBudget;
+    private Spinner spinnerAccountSelect;
+    private Button saveBtnB;
+    private String catName;
+    private String categoryTitle;
+    private String clickedAccountName;
+
+    public interface SaveInput {
+        void save(String value, String categoryTitle, String clickedAccountName);
+    }
+
+    private SaveInput saveInput;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_budget_popup, container, false);
+        Log.d(tag, "In the BudgetDialog event");
+        inputAmountText = view.findViewById(R.id.amount);
+        saveBtnB = view.findViewById(R.id.savebtnB);
+        spinnerBudget = view.findViewById(R.id.spinnerBudget);
+        spinnerBudget.setAdapter(AppConstants.expenseAdapter);
+
+        AppConstants.fillAccountSpinner(getContext());
+        spinnerAccountSelect = view.findViewById(R.id.spinnerAccountSelectBudget);
+        spinnerAccountSelect.setAdapter(AppConstants.accountAdapter);
+
+        spinnerBudget.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                CategoryRowItem clickedItem = (CategoryRowItem) adapterView.getItemAtPosition(i);
+                catName = clickedItem.getmCategoryName();
+                categoryTitle = clickedItem.getmCategoryName();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        spinnerAccountSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                CategoryRowItem clickedItem = (CategoryRowItem) adapterView.getItemAtPosition(i);
+                clickedAccountName = clickedItem.getmCategoryName();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        saveBtnB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //This is the totalamount money
+                String inputAmount = inputAmountText.getText().toString().trim();
+                getDialog().dismiss();
+                saveInput.save(inputAmount, categoryTitle, clickedAccountName);
+            }
+        });
+
+        return view;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        try {
+            saveInput = (BudgetDialog.SaveInput) getTargetFragment();
+        } catch (ClassCastException e) {
+            Log.e(tag, e.toString() + " in CreateAccountDialog");
+        }
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(tag, "In the onDestroyView() event");
+        if (!AppConstants.accountExist()) Objects.requireNonNull(getActivity()).finish();
+    }
+}
